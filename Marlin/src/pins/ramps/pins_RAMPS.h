4763dae41d7e979a986970020978e1dbf31dbf30
Jason Smith <jason.inet@gmail.com>
2019-12-24 00:59:47 -0800

    Revert RAMPS serial pin changes

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -322,7 +322,7 @@
   //
 
   #ifndef X_SERIAL_TX_PIN
-    #define X_SERIAL_TX_PIN  63
+    #define X_SERIAL_TX_PIN  40
   #endif
   #ifndef X_SERIAL_RX_PIN
     #define X_SERIAL_RX_PIN  63
@@ -335,7 +335,7 @@
   #endif
 
   #ifndef Y_SERIAL_TX_PIN
-    #define Y_SERIAL_TX_PIN  64
+    #define Y_SERIAL_TX_PIN  59
   #endif
   #ifndef Y_SERIAL_RX_PIN
     #define Y_SERIAL_RX_PIN  64
@@ -348,7 +348,7 @@
   #endif
 
   #ifndef Z_SERIAL_TX_PIN
-    #define Z_SERIAL_TX_PIN  65
+    #define Z_SERIAL_TX_PIN  42
   #endif
   #ifndef Z_SERIAL_RX_PIN
     #define Z_SERIAL_RX_PIN  65
@@ -361,7 +361,7 @@
   #endif
 
   #ifndef E0_SERIAL_TX_PIN
-    #define E0_SERIAL_TX_PIN 66
+    #define E0_SERIAL_TX_PIN 44
   #endif
   #ifndef E0_SERIAL_RX_PIN
     #define E0_SERIAL_RX_PIN 66
