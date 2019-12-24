043f3c1266753c0c7c22de01c67f3d24875a2b5f
Jason Smith <jason.inet@gmail.com>
2019-12-24 00:40:58 -0800

    Reduce Z current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1912,7 +1912,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       580
+    #define Z_CURRENT       400
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
