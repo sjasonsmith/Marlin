cfd2077d93c5874f08bfcb45b89953d5c8fdd001
Jason Smith <jason.inet@gmail.com>
2020-04-18 14:23:07 -0700

    Configure for e3d hotend w/ Titan extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -406,7 +406,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 400.0, 471.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 400.0, 418.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
