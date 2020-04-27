c5f0dcc3e478b46ba88735bb393ec2dacdc31552
Jason Smith <jason.inet@gmail.com>
2020-04-26 23:55:10 -0700

    Configure for 1.75mm BMG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -145,7 +145,7 @@
 #define EXTRUDERS 1
 
 // Generally expected filament diameter (1.75, 2.85, 3.0, ...). Used for Volumetric, Filament Width Sensor, etc.
-#define DEFAULT_NOMINAL_FILAMENT_DIA 3.0
+#define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
 
 // For Cyclops or any "multi-extruder" that shares a single nozzle.
 //#define SINGLENOZZLE
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 400.0, 418.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 400.0, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1063,7 +1063,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
